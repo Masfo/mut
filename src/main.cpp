2abf25fa3324d6c5045fcbd78fef94ecb2cd1896
Toni Mäkinen <masfonos@gmail.com>
2025-08-11 05:43:00 +0300

    use deckards execute_process

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include <shellapi.h>
 
 import std;
@@ -19,98 +19,7 @@ std::string input(std::string_view ask)
 	return ret;
 }
 
-struct CommandResult
-{
-	std::chrono::duration<float> process_time;
-	std::optional<i32>           exit_code{};
-	std::string                  stdout_str;
-	std::string                  stderr_str;
-};
 
-CommandResult run_command(const std::string &command, fs::path folder = fs::current_path())
-{
-	CommandResult command_result;
-
-	// Create pipe and read stdout/stderr to string
-
-	PROCESS_INFORMATION pi{};
-	STARTUPINFOA        si = {sizeof(STARTUPINFO)};
-	si.dwFlags             = STARTF_USESTDHANDLES;
-	si.hStdOutput          = GetStdHandle(STD_OUTPUT_HANDLE);
-	si.hStdInput           = GetStdHandle(STD_INPUT_HANDLE);
-	si.hStdError           = GetStdHandle(STD_ERROR_HANDLE);
-
-	// std::string args = "cmd /c " + command;
-	std::string args = "cmd /c " + command;
-	args             = "";
-
-	if (command.empty())
-	{
-		std::println(std::cerr, "no executable set");
-		return {};
-	}
-
-	std::println("");
-
-	auto start = std::chrono::system_clock::now();
-
-	// ExpandEnvironmentStringsA
-	// When you use cmd.exe, it performs the expansion for you. CreateProcess
-	// does not so you will need to do it before calling CreateProcess.
-	// Alternatively you could use ShellExecute which will expand environment strings.
-
-	// void *pEnv = nullptr;
-	// char  cmd[4096];
-
-	// strcpy(cmd,command);
-	// ExpandEnvironmentStringsA(command.data(), cmd, sizeof(cmd) - 1);
-
-	if (auto result = CreateProcessA(
-			nullptr, (LPSTR)command.c_str(), nullptr, nullptr, TRUE, CREATE_UNICODE_ENVIRONMENT, 0, folder.string().c_str(), &si, &pi);
-		result == 0)
-	{
-		command_result.exit_code = GetLastError();
-		std::println(std::cerr, "Could not run '{}' - {}", command, *command_result.exit_code);
-
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
-
-		return command_result;
-	}
-
-	// TODO: Add timeout parameter
-	u32 timeout = 1000;
-	if (auto wait = WaitForSingleObject(pi.hProcess, timeout); wait != 0)
-	{
-		std::println("");
-		if (wait == WAIT_TIMEOUT)
-		{
-			std::println("Timeout: {:x}", GetLastError());
-		}
-		else
-		{
-			std::println("Failed: {:x}", GetLastError());
-		}
-
-		command_result.exit_code = GetLastError();
-		return command_result;
-	}
-
-	auto end = std::chrono::system_clock::now();
-	std::println("");
-
-	//
-	DWORD exit_code = 0;
-	if (GetExitCodeProcess(pi.hProcess, &exit_code) != 0)
-		command_result.exit_code = exit_code;
-
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-
-	command_result.process_time = end - start;
-
-	return command_result;
-}
 
 bool match(std::string_view pattern, std::string_view str)
 {
@@ -168,9 +77,17 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	std::string arguments;
+	 for (int i = 1; i < argc; i++)
+	{
+		arguments += argv[i];
+		if (i < argc - 1)
+			arguments += " ";
+	}
+	
 
-	auto result = run_command(argv[1]);
-	std::println("Took: {} - {}", result.process_time, result.exit_code ? *result.exit_code : 666);
+	auto result = system::execute_process(arguments);
+	std::println("Took: {} - exit result: {}", result.elapsed_time.count(), result.exit_code);
 
 	for (int count = 0; count < argc; count++)
 		std::println("  argv[{}]   {}", count, argv[count]);
@@ -180,16 +97,16 @@ int main(int argc, char **argv)
 	for (const auto &p : params | std::views::join_with(' '))
 		joined_params += p;
 
-	std::string lineInput;
-	while (std::getline(std::cin, lineInput))
-	{
-		if (lineInput.empty())
-			break;
-
-		std::println("PIPED: {}", lineInput);
-	}
-
-	std::println("P: {}", joined_params);
+	//std::string lineInput;
+	//while (std::getline(std::cin, lineInput))
+	//{
+	//	if (lineInput.empty())
+	//		break;
+	//
+	//	std::println("PIPED: {}", lineInput);
+	//}
+	//
+	//std::println("P: {}", joined_params);
 
 	// mut tests.exe "cls && tests"
 
